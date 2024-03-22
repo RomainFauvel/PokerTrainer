452d209adca76b47ada2fc04611bff9b30e34594
Romain <r0fauvel@gmail.com>
2024-03-22 14:00:02 +0100

    modif GameTree constructeur et ajout : GameTree.initialise(args)

--- code/Python/Commande/GameTree.py
+++ code/Python/Commande/GameTree.py
@@ -13,28 +13,29 @@ class GameTree:
     def __new__(cls, *args, **kwargs): #Creation d'un singleton pour avoir une seule sdd partagee entre les classes
         if cls._instance is None:
             cls._instance = super().__new__(cls)
-            cls._instance.__initialized = False
         return cls._instance
 
     def __init__(self,filePath=None,playerHand=None,flop=None,river=None,turn=None): #initialisation de la classe
-        if self.__initialized: return
 
-        if(not(filePath==None and playerHand==None)):
-            with open(filePath,'r') as json_file:
-                self.data=json.load(json_file)
-            self.playerHand=playerHand
-            self.flop=flop
-            self.river=river
-            self.turn=turn
-            self.__initialized = True
-            print("GameTree initialized")
-            self.compteur = 0
+        if(not(filePath==None and playerHand==None and flop==None and river==None and turn==None)):
+            self.initialise(filePath,playerHand,flop,river,turn)
+    
+    #Peux etre acceder depuis l exterieur sans initialiser un objet (exemple: GameTree.initialise(filePath,playerHand,flop,river,turn))
+    @classmethod
+    def initialise(cls,filePath,playerHand,flop,river,turn):
+        if(cls._instance is None):
+            cls._instance = cls()
+        with open(filePath) as f:
+            cls._instance.data = json.load(f)
+        cls._instance.setPlayerHand(playerHand)
+        cls._instance.setFlop(flop)
+        cls._instance.setRiver(river)
+        cls._instance.setTurn(turn)
 
     def rolloutToInit(self,file,val):
         self.__initialized = False
         self.__init__(file,val)
 
-        
         
 
     def play(self,todo): # se deplace dans l arbre en prenant en compte l'action réalisée par le joueur/Ordi
@@ -60,12 +61,51 @@ class GameTree:
     #return round (i.e pre-flop,flop,turn,river)
     def getRound(self):
         return self.compteur
-        
-
-    def getPlayerPossiblities(self): # renvoie les proba de chaque actions possibles sous forme de dictionnaire avec les actions pour clés 
-        dicoProba={}
-        for i in range(len(self.data["strategy"]["actions"])):
-            dicoProba.update({self.data["strategy"]["actions"][i]:round(self.data["strategy"]["strategy"][self.playerHand][i]*100,3)})
-        return dicoProba
-
     
+    def getData(self):
+        return self.data
+
+    def getPlayerHand(self):
+        return self.playerHand
+
+    def getFlop(self):
+        return self.flop
+
+    def getRiver(self):
+        return self.river
+
+    def getTurn(self):
+        return self.turn
+    
+    # setters
+
+    def setPlayerHand(self, playerHand):
+        self.playerHand = playerHand
+
+    def setFlop(self, flop):
+        self.flop = flop
+
+    def setRiver(self, river):
+        self.river = river
+
+    def setTurn(self, turn):
+        self.turn = turn
+
+    def to_string(self):
+        return "GameTree: playerHand: " + str(self.playerHand) + " flop: " + str(self.flop) + " river: " + str(self.river) + " turn: " + str(self.turn)
+
+
+if(__name__=="__main__"):
+    gt0=GameTree()
+    gt1=GameTree(filePath="Ressources/output_strategyTest.json",playerHand="playerHand",flop="Theo",river="river",turn="turn")
+    print(gt0.to_string())
+    print(gt1.to_string())
+    GameTree.initialise("Ressources/output_strategyTest.json","","","Corentin","")
+    print(gt0.to_string())
+    print(gt1.to_string())
+    gt2=GameTree(filePath="Ressources/output_strategyTest.json",playerHand="playerHand",flop="Theo",river="river",turn="turn")
+    gt3=GameTree()
+    print(gt0.to_string())
+    print(gt1.to_string())
+    print(gt2.to_string())
+    print(gt3.to_string())
